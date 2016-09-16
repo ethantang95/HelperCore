2ed286fa4e52e8a28e205ec515d6b1552127d2fd
Ethan Tang <eytang@uwaterloo.ca>
2016-09-16 13:24:56 -0400

    Initial commit

---
+++ HelperCore/HelperCore/Optional/Optional.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperCore.Optional
+{
+    /// <summary>
+    /// This class was made specfically to fight against the idea of nulls that exists in the langauge.
+    /// This is because nulls are a horrible way of communicating a value is missing, and the location of where the null was is not really explicit.
+    /// Therefore, this class exists to better communicate and mitigate the null problem.
+    /// This class forces the person to check whether a value is null or not before proceeding.
+    /// 
+    /// The object inside this class should not be mutable as it comes from an external source or somewhere else.
+    /// This would imply that the functions here will not mutate the value that was passed in here initially.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Optional<T>
+    {
+        //initalization methods
+        /// <summary>
+        /// Create an optional with the type and the given object
+        /// </summary>
+        /// <param name="obj">the object</param>
+        /// <returns>An optional object of the type the object is, along with the object wrapped in it</returns>
+        public static Optional<T> From(T obj)
+        {
+            return new Optional<T>(obj);
+        }
+
+        /// <summary>
+        /// Create an optional with the type but with a null object
+        /// </summary>
+        /// <returns>An object of the type but with null for the value</returns>
+        public static Optional<T> FromNull()
+        {
+            return new Optional<T>(null);
+        }
+        
+        /// <summary>
+        /// Get the value that is inside the optional. If the value is null, a null reference exception is thrown
+        /// </summary>
+        public T Value {
+            get
+            {
+                if (_value == null)
+                {
+                    throw new NullReferenceException("Attempted to get a value from an Optional<> that ended up being a null");
+                }
+                else
+                {
+                    return (T)_value;
+                }
+            }
+        }
+
+        private object _value;
+
+        private Optional(object obj)
+        {
+            _value = obj;
+        }
+
+        /// <summary>
+        /// Check to see if the value is present or not
+        /// </summary>
+        /// <returns>Whether the value is present or not</returns>
+        public bool IsPresent()
+        {
+            return _value != null;
+        }
+
+        /// <summary>
+        /// Returns either the value if present or the other if it's not
+        /// </summary>
+        /// <param name="other">the value to be returned if there are no values present, it can be null</param>
+        /// <returns>the value if present, otherwise the other</returns>
+        public T OrElse(T other)
+        {
+            return _value != null ? Value : other;
+        }
+
+        /// <summary>
+        /// Returns either the value if present or default of the type if it's not
+        /// </summary>
+        /// <returns>the value if present, otherwise default(T)</returns>
+        public T OrDefault()
+        {
+            return _value != null ? Value : default(T);
+        }
+    }
+}
---
+++ HelperCore/HelperCoreTests/OptionalTests/TestClass.cs
@@ -0,0 +1,205 @@
+﻿using HelperCore.Optional;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperCoreTests.OptionalTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestCreatingOptionalFromObject()
+        {
+            var dummy = Optional<DummyClass>.From(new DummyClass());
+        }
+
+        [Test]
+        public void TestCreatingOptionalFromValue()
+        {
+            var value = Optional<int>.From(10);
+        }
+
+        [Test]
+        public void TestGettingValidOptionalObject()
+        {
+            var dummy = CreateDummy();
+
+            var optionalDummy = Optional<DummyClass>.From(dummy);
+
+            var optionalVal = optionalDummy.Value;
+
+            Assert.AreEqual(dummy, optionalVal);
+        }
+
+        [Test]
+        public void TestGettingValidOptionalValue()
+        {
+            var optionalValue = Optional<int>.From(10);
+
+            var optionalVal = optionalValue.Value;
+
+            Assert.AreEqual(10, optionalVal);
+        }
+
+        [Test]
+        public void TestGettingNullObject()
+        {
+            var optionalDummy = Optional<DummyClass>.FromNull();
+            Assert.Throws<NullReferenceException>(() => { var val = optionalDummy.Value; });
+
+            var optionalDummy2 = Optional<DummyClass>.From(null);
+            Assert.Throws<NullReferenceException>(() => { var val = optionalDummy2.Value; });
+        }
+
+        [Test]
+        public void TestGettingNullValue()
+        {
+            var optionalValue = Optional<int>.FromNull();
+            Assert.Throws<NullReferenceException>(() => { var val = optionalValue.Value; });
+        }
+
+        [Test]
+        public void TestCheckingContainsObject()
+        {
+            var optionalDummy = Optional<DummyClass>.From(new DummyClass());
+
+            Assert.True(optionalDummy.IsPresent());
+        }
+
+        [Test]
+        public void TestCheckingContainsValue()
+        {
+            var optionalValue = Optional<int>.From(10);
+
+            Assert.True(optionalValue.IsPresent());
+        }
+
+        [Test]
+        public void TestCheckingContainsNullObject()
+        {
+            var optionalDummy = Optional<DummyClass>.FromNull();
+            Assert.False(optionalDummy.IsPresent());
+
+            var optionalDummy2 = Optional<DummyClass>.From(null);
+            Assert.False(optionalDummy2.IsPresent());
+        }
+
+        [Test]
+        public void TestCheckingContainsNullValue()
+        {
+            var optionalValue = Optional<int>.FromNull();
+            Assert.False(optionalValue.IsPresent());
+        }
+
+        [Test]
+        public void TestOrElseHasObject()
+        {
+            var dummy = CreateDummy();
+            var elseDummy = CreateDummy(2);
+
+            var optionalDummy = Optional<DummyClass>.From(dummy);
+
+            Assert.AreEqual(dummy, optionalDummy.OrElse(elseDummy));
+        }
+
+        [Test]
+        public void TestOrElseHasValue()
+        {
+            var optionalValue = Optional<int>.From(10);
+
+            Assert.AreEqual(10, optionalValue.OrElse(5));
+        }
+
+        [Test]
+        public void TestOrElseNoObject()
+        {
+            var elseDummy = CreateDummy(2);
+
+            var optionalDummy = Optional<DummyClass>.FromNull();
+
+            Assert.AreEqual(elseDummy, optionalDummy.OrElse(elseDummy));
+
+            var optionalDummy2 = Optional<DummyClass>.From(null);
+
+            Assert.AreEqual(elseDummy, optionalDummy2.OrElse(elseDummy));
+        }
+
+        [Test]
+        public void TestOrElseNoValue()
+        {
+            var optionalValue = Optional<int>.FromNull();
+
+            Assert.AreEqual(5, optionalValue.OrElse(5));
+        }
+
+        [Test]
+        public void TestDefaultHasObject()
+        {
+            var dummy = CreateDummy();
+
+            var optionalDummy = Optional<DummyClass>.From(dummy);
+
+            Assert.AreEqual(dummy, optionalDummy.OrDefault());
+        }
+
+        [Test]
+        public void TestDefaultHasValue()
+        {
+            var optionalValue = Optional<int>.From(10);
+
+            Assert.AreEqual(10, optionalValue.OrDefault());
+        }
+
+        [Test]
+        public void TestDefaultNoObject()
+        {
+            var optionalDummy = Optional<DummyClass>.FromNull();
+            Assert.AreEqual(default(DummyClass), optionalDummy.OrDefault());
+
+            var optionalDummy2 = Optional<DummyClass>.From(null);
+            Assert.AreEqual(default(DummyClass), optionalDummy2.OrDefault());
+        }
+
+        [Test]
+        public void TestDefaultNoValue()
+        {
+            var optionalValue = Optional<int>.FromNull();
+            Assert.AreEqual(default(int), optionalValue.OrDefault());
+        }
+
+        [Test]
+        public void TestChainning()
+        {
+            var dummy = CreateDummy();
+            dummy.OptionalString = Optional<string>.From("world");
+
+            var optionalDummy = Optional<DummyClass>.From(dummy);
+
+            Assert.AreEqual("world", optionalDummy.Value.OptionalString.Value);
+        }
+
+        private DummyClass CreateDummy()
+        {
+            return CreateDummy(0);
+        }
+
+        private DummyClass CreateDummy(int value)
+        {
+            var dummy = new DummyClass();
+            dummy.IntValue = value;
+            dummy.StringValue = "hello";
+            return dummy;
+        }
+    }
+
+    internal class DummyClass
+    {
+        public int IntValue { get; set; }
+        public string StringValue { get; set; }
+        public Optional<string> OptionalString { get; set; }
+    }
+}
